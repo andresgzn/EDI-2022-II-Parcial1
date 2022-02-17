84991afbb9da52bb443053cd6534686785221811
Andrés González <a324846@alumnos.uaslp.mx>
2022-02-17 09:50:44 -0600

    17/02/22

--- EjerciciosClase/Ejercicio11_Funciones.c
+++ EjerciciosClase/Ejercicio11_Funciones.c
@@ -1,3 +1,21 @@
+/*
+ Las funciones son un pedazo del codigo que realizan un proceso:
+ Funcion:
+   -Que proceso va a realizar
+   -Que necesita para trabajar
+   -Que me va a regresar la informacion
+
+ Estructura:
+   1. Encabezado - primer linea
+      -Tipo de dato, lo que va a regresar
+       (si no regresa nada es void)
+      -Nombre de la funcion
+      -Entre parentesis van los parametros
+      -Llaves, cuerpo de la funcion
+      -Si la funcion tiene un tipo de retorno diferente de void
+       debe de llevar un return
+ */
+
 #include <stdio.h>
 
 void imprime_pares(int inferior, int superior);

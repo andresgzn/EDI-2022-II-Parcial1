280ea362dc7f647c4e5107315d91cf1c70eb18da
Andrés González <a324846@alumnos.uaslp.mx>
2022-02-28 19:27:52 -0600

    28/02/22

---
+++ Tarea7_E1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define TAM 100
+
+void pideOracion(char oracion[TAM]);
+void pideCaracter(char *c);
+int calculaCaracter(char oracion[TAM], char caracter);
+
+int main()
+{
+    int x;
+    char oracion[TAM];
+    char c;
+
+    printf("------- CADENAS -------\n");
+
+    pideOracion(oracion);
+    pideCaracter(&c);
+
+    printf("\n");
+
+    x = calculaCaracter(oracion, c);
+
+
+    printf("----------------------------------------------\n");
+    printf("La letra '%c' aparece %d veces en la cadena\n", c, x);
+    printf("----------------------------------------------\n");
+
+    return 0;
+}
+
+void pideOracion(char oracion[TAM])
+{
+    printf(" Cadena: ");
+    scanf("%[^\n]", oracion);
+}
+
+void pideCaracter(char *c)
+{
+    printf("Caracter: ");
+    scanf("%s", &*c);
+}
+
+int calculaCaracter(char oracion[TAM], char caracter)
+{
+    int contador=0;
+
+    for (int i = 0; i < TAM; ++i)
+    {
+        if(oracion[i] == caracter)
+        {
+            contador += 1;
+        }
+    }
+
+    return (contador);
+}
